723328955460bcef73f6c1fda216f8774e932490
Affo Amadou <hamadouaffo@gmail.com>
2022-07-05 14:52:42 +0200

    prismic

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', "pug")
 app.get('/', (req, res) => {
-//!   res.send('Hello World!')
-res.render()
+    //!   res.send('Hello World!')
+    res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+    //!   res.send('Hello World!')
+    res.render('pages/about')
+})
+
+app.get('/detail/:uid', (req, res) => {
+    //!   res.send('Hello World!')
+    res.render('pages/detail')
+})
+
+app.get('/collections', (req, res) => {
+    //!   res.send('Hello World!')
+    res.render('pages/collections')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 //*Creazione del backend che gestisce l'indirizzamento delle pagine
