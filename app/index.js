409ebce7e7fd495588df51b29c7d4fe6ca0308d8
Affo Amadou <hamadouaffo@gmail.com>
2022-07-28 14:03:57 +0200

    js start

--- app/index.js
+++ app/index.js
@@ -1,3 +1,35 @@
-import pp from 'images/aa.jpg'
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
+ //* IMPORTO GLI INDEX PRESENTI NELLE CARTELLE
 
-console.log(pp)
+class App{
+constructor(){
+    this.createContent()
+    this.createPages()
+}
+
+createContent(){
+
+    //?block variables      - var template = 'home'   block content  QUESTA PARTE DEL FILE PUG
+    this.content = document.querySelector('.content')
+    this.template = this.content.getAttribute('data-template')
+   //? Cosi ottengo la pagina in cui mi trovo GENIALEEE
+}
+
+createPages(){
+    this.pages = {
+        about: new About(),
+        home: new Home(),
+        collections: new Collections(),
+        detail: new Detail(),
+        
+    }
+
+    this.page = this.pages[this.template] //!INSERISCO LA PAGINA IN CUI SONO ATTUALMENTE
+    console.log(this.page)
+}
+}
+
+new App()
---
+++ app/pages/Home/index.js
@@ -0,0 +1,10 @@
+import Page from "../../classes/Page";
+
+export default class Home extends Page{
+    constructor(){
+      super({
+        id:'home' //!Passo alla classe page l'id
+      })
+        console.log("HHHO")
+    }
+}
